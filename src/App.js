56edab589ccb9b44d6dd9450e8bc7bf44137f1cd
Gracerue <munyaradzi045@gmail.com>
2023-07-18 21:37:47 +0200

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import Greeting from './components/Greeting'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Greeting from './components/Greeting';
 
 export default function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Routes> 
-          <Route path="/" element={<Greeting/>} /> 
-        </Routes> 
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
       </BrowserRouter>
     </Provider>
-  )
+  );
 }
--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchGreeting } from "../redux/greetingActions";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreeting } from '../redux/greetingActions';
 
 function Greeting() {
   const dispatch = useDispatch();
   const { greeting, loading, error } = useSelector((state) => state.greeting);
 
   const handleFetchGreeting = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     dispatch(fetchGreeting());
   };
 
   return (
     <div>
-      <button onClick={handleFetchGreeting} disabled={loading}>
+      <button type='button' onClick={handleFetchGreeting} disabled={loading}>
         Fetch Greeting
       </button>
       {loading ? (
