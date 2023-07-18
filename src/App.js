0ae70571c15b7ee077b142876bb2a9c3e244dca1
Gracerue <munyaradzi045@gmail.com>
2023-07-18 16:32:58 +0200

    Connect the rails Api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import Greeting from './components/Greeting'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes> 
+          <Route path="/" element={<Greeting/>} /> 
+        </Routes> 
+      </BrowserRouter>
+    </Provider>
+  )
 }
-
-export default App;
---
+++ src/components/Greeting.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGreeting } from "../redux/greetingActions";
+
+function Greeting() {
+  const dispatch = useDispatch();
+  const { greeting, loading, error } = useSelector((state) => state.greeting);
+
+  const handleFetchGreeting = (e) => {
+    e.preventDefault()
+    dispatch(fetchGreeting());
+  };
+
+  return (
+    <div>
+      <button onClick={handleFetchGreeting} disabled={loading}>
+        Fetch Greeting
+      </button>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <p>Greeting: {greeting}</p>
+      )}
+    </div>
+  );
+}
+
+export default Greeting;
